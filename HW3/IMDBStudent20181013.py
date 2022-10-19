b609c3626ae7f4a997635b26d8f7891eb6091144
youkyoungJung <yyk834@naver.com>
2022-10-18 21:01:34 -0700

    IMDB Genre count Program

---
+++ HW3/IMDBStudent20181013.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import sys
+
+#print(sys.argv[1])
+genre = []
+genres = dict()
+
+try:
+	with open(sys.argv[1], "rt") as f:
+		
+		for line in f:
+			str_arr = line.split("::")
+			genre.append(str_arr[2].strip())
+	
+	for i in genre:
+		if i in genres:
+			genres[i] += 1
+		else:
+			genres[i] = 1
+
+	
+	with open(sys.argv[2], "wt") as f:
+		for gen,count in genres.items():
+			f.write(str(gen)+" "+ str(count)+"\n")
+			
+
+except FileNotFoundError:
+	print("데이터 파일이 없습니다")
+
+
+
+		
+		
