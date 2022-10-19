802f82ec141f1a85e02e222cce5126dfb7d4b203
youkyoungJung <yyk834@naver.com>
2022-10-18 23:12:21 -0700

    Processing data with Uber data

---
+++ HW3/UBERStudent20181013.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import sys
+import datetime
+
+base_n = []
+date = []
+act_veh = []
+trips = []
+t = []
+def get_days(yyyy, mm, dd):
+	days = ["MON", "TUE","WED", "THU","FRI","SAT","SUN"]
+	#print(days[datetime.date(yyyy,mm,dd).weekday()])
+	return days[datetime.date(yyyy,mm,dd).weekday()]
+
+try:
+	with open(sys.argv[1], "rt") as f:
+		for line in f:
+			data = line.split(",")
+			base_n.append(data[0].strip())
+			mm = int(data[1].split("/")[0])
+			dd = int(data[1].split("/")[1])
+			yyyy = int(data[1].split("/")[2]) 
+			date.append(get_days(yyyy,mm,dd))
+			act_veh.append(data[2].strip())
+			trips.append(data[3].strip())
+		
+		#print(act_veh)
+		#print(date)
+		
+	with open(sys.argv[2], "wt") as f:
+		for region, day, vehicles, trips in zip(base_n, date, act_veh, trips):
+			f.write(str(region) + " " + str(day) + " " + str(vehicles) + " " + str(trips) + "\n")
+		
+		
+			
+		
+except FileNotFoundError:
+	print("데이터 파일이 없습니다")
